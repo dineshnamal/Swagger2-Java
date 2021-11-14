bf9549a2f9a9abc165e7943b8c87e18affa2b21a
namdinesh <namdinesh@users.noreply.github.com>
2021-11-14 15:12:40 +1100

    first commit

---
+++ src/main/java/com/example/swaggercodegenjava/SwaggerCodegenJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.swaggercodegenjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerCodegenJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerCodegenJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/swaggercodegenjava/SwaggerCodegenJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swaggercodegenjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerCodegenJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
